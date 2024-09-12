adf5acc0ba95363996cfc56a11502ac5e1496354
Naidan <naidan.salvador2@hotmail.com>
2024-09-12 16:27:56 +0100

    3. Functional Components

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+// This is a simple component that will be rendered at the top of the page.
+// use command shift R to do this
+
+const Header = () => {
+  return (
+    <header>
+        <h1>Groceries List</h1>
+    </header>
+  )
+}
+
+export default Header
