b71669f9ad7e1915521e74c334f2ec2ecc9a6353
Naidan <naidan.salvador2@hotmail.com>
2024-09-12 16:42:32 +0100

    4.Applying CSS styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
@@ -15,11 +13,5 @@ function App() {
     </div>
   );
 }
-
-// jsx renders data as text when we enter it in curly braces
-// the curly braces tell us that it is a javascript expression
-// we cannnot however express an object in the curly braces
-// a boolean will also not be rendered
-// shift option a will comment out a block of code in vscode
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,6 @@
-// This is a simple component that will be rendered at the top of the page.
-// use command shift R to do this
-
 const Header = () => {
+    
+
   return (
     <header>
         <h1>Groceries List</h1>
@@ -9,4 +8,4 @@ const Header = () => {
   )
 }
 
-export default Header
+export default Header;
