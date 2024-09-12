4a530e3da45ca201828b27d38b63c684d211d95e
Naidan <naidan.salvador2@hotmail.com>
2024-09-12 16:59:03 +0100

    5.Click Events

--- src/Content.js
+++ src/Content.js
@@ -4,11 +4,28 @@ const Content = () => {
         const int = Math.floor(Math.random() * 3);
         return names[int];
       };
+      const handleClick = () =>{
+        console.log('You Clicked It');
+      }
+      const handleClick2 = (name) =>{
+        console.log(`${name} was clicked`);
+      }
+      const handleClick3 = (e) =>{
+        console.log(e);
+      }
+
     return (
         <main>
-            <p>
+            <p onDoubleClick={handleClick}>
                 Hello {handleNameChange()}!
             </p>
+            <button onClick={handleClick}>Click It</button>
+            {/* We dont have the operators () after handle click as when we dont it is called immediately */}
+            <button onClick={() => handleClick2('Dave')}>Click It</button>
+             {/* We Need an anonymous function in handleClick2. Now handle click 2 can be called and we can pass in our name */}
+            
+            <button onClick={(e) => handleClick3(e)}>Click It</button>            
+            {/* We get the buttons details bug we can make it even more specific using . */}
         </main>
     )
 }
