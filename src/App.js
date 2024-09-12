9d6766ec78f063cd43228d8a7e81b8f1fa3625fb
Naidan <naidan.salvador2@hotmail.com>
2024-09-12 15:24:49 +0100

    2.App And JSX

--- src/App.js
+++ src/App.js
@@ -2,24 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleNameChange = () => {
+    const names = ['Bob', 'Kevin', 'Dave'];
+    const int = Math.floor(Math.random() * 3);
+    return names[int];
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to see Changes.
+          Hello {handleNameChange()}!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
 }
+
+// jsx renders data as text when we enter it in curly braces
+// the curly braces tell us that it is a javascript expression
+// we cannnot however express an object in the curly braces
+// a boolean will also not be rendered
+// shift option a will comment out a block of code in vscode
 
 export default App;
